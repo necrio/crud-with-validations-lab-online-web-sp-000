dfaadd6c13be5e5fef2dd32a2bc6077a8118c767
Keith Isham <kisham8899@gmail.com>
2020-01-17 11:43:30 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     if @song.update(song_params)
-      redirect_to song_path(@song)
+      redirect_to songs_path(@song)
     else
       render :edit
     end
