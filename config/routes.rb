9f6b0ed79a971947fbeeac0808814c3eab279ec9
Keith Isham <kisham8899@gmail.com>
2020-01-17 11:44:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'songs/index'
-
-  get 'songs/show'
-
-  get 'songs/new'
-
-  get 'songs/create'
-
-  get 'songs/update'
-
-  get 'songs/edit'
-
-  get 'songs/destroy'
-
+  resources :songs 
+  root 'songs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
